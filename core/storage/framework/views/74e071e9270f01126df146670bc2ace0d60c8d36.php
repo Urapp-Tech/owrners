276d824949385e5d93fad227eb8de9943e8c2b70
Shoaib Ahmed <shoaib@urapptech.com>
2024-06-11 17:24:28 +0500

    Initialize Project

---
+++ core/storage/framework/views/74e071e9270f01126df146670bc2ace0d60c8d36.php
@@ -0,0 +1,2 @@
+<script src="<?php echo e(asset('assets/common/js/select2.min.js')); ?>"></script>
+<?php /**PATH E:\laragon\www\owrners\core\resources\views/components/select2/select2-js.blade.php ENDPATH**/ ?>
