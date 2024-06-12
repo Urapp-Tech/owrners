fed574cb85251036f471550177726e815e4ebcc7
Shoaib Ahmed <shoaib@urapptech.com>
2024-06-12 10:55:20 +0500

    ignore Caches

--- core/storage/framework/views/74e071e9270f01126df146670bc2ace0d60c8d36.php
+++
@@ -1,2 +0,0 @@
-<script src="<?php echo e(asset('assets/common/js/select2.min.js')); ?>"></script>
-<?php /**PATH E:\laragon\www\owrners\core\resources\views/components/select2/select2-js.blade.php ENDPATH**/ ?>
--- core/storage/framework/views/7dd6ad6c26af56df0d80647215b24d3b467b71d1.php
+++
@@ -1,2 +0,0 @@
-<link rel="stylesheet" href="<?php echo e(asset('assets/common/css/select2.min.css')); ?>">
-<?php /**PATH E:\laragon\www\owrners\core\resources\views/components/select2/select2-css.blade.php ENDPATH**/ ?>
